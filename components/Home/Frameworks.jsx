0f3bc035a50d6ed767630ac6eb670dffb5fa2db0
sikander-afzaal <sikander.aaa@gmail.com>
2023-03-09 22:41:34 +0500

    updated

--- components/Home/Frameworks.jsx
+++ components/Home/Frameworks.jsx
@@ -4,7 +4,7 @@ const Frameworks = () => {
   return (
     <div className="w-full mt-[70px] sm:mt-[100px] md:mt-[150px] flex justify-center items-center">
       <div className="bg-darkBlue rounded-[10vw] py-[40px] md:py-[80px] px-5 sm:px-[60px] lg:px-[120px] flex justify-center items-center flex-col w-[90%]">
-        <h2 className="text-white font-medium text-[40px] sm:text-[60px] lg:text-[80px]">
+        <h2 className="text-white font-dumbledore font-medium text-[40px] sm:text-[60px] lg:text-[80px]">
           Frameworks
         </h2>
         <div className="mt-[50px] lg:mt-[100px] flex justify-center items-center w-full flex-col gap-20">
@@ -41,7 +41,7 @@ const FrameBox = ({ img, title, para }) => {
     <div className="text-center lg:text-left flex lg:flex-row flex-col justify-start items-center gap-6 sm:gap-10 w-full">
       <Image src={img} width={192} height={192} alt="..." />
       <div className="flex justify-start items-center lg:items-start flex-col gap-2">
-        <h3 className="text-white text-[50px] sm:text-[70px] leading-[1] font-medium ">
+        <h3 className="text-white font-dumbledore text-[50px] sm:text-[70px] leading-[1] font-medium ">
           {title}
         </h3>
         <p className="text-white text-xl sm:text-[24px]">{para}</p>
